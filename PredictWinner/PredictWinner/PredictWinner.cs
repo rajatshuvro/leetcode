8563abeae95c51e869a481b47d858573d0f649fb
Rajat Roy <rroy1@illumina.com>
2017-11-25 13:14:00 -0800

    iterative solution that is at 100 percentile. O(n) space

--- PredictWinner/PredictWinner/PredictWinner.cs
+++ PredictWinner/PredictWinner/PredictWinner.cs
@@ -19,48 +19,35 @@ namespace PredictWinner
         {
             var sol = new PredictWinner();
             var result = sol.PredictTheWinner(new[] { 2 });
-            result &= sol.PredictTheWinner(new[] { 3, 4});
+            result &= sol.PredictTheWinner(new[] { 3, 4 });
             result &= !sol.PredictTheWinner(new[] { 1, 5, 2 });
             result &= sol.PredictTheWinner(new[] { 1, 5, 233, 7 });
             result &= !sol.PredictTheWinner(new[] { 2, 4, 55, 6, 8 });
-            result &= sol.PredictTheWinner(new[] { 1, 0, 0, 1, 1, 1, 1231, 0, 1, 1, 1, 1, 1, 1, 12, 0, 0, 0, 0, 0});
+            result &= sol.PredictTheWinner(new[] { 1, 0, 0, 1, 1, 1, 1231, 0, 1, 1, 1, 1, 1, 1, 12, 0, 0, 0, 0, 0 });
             result &= sol.PredictTheWinner(new[] { 10, 17, 11, 16, 17, 9, 14, 17, 18, 13, 11, 4, 17, 18, 15, 3, 13, 10, 6, 10 });
 
             return result;
         }
 
-        static int[,] _firstPlayerLead;
-
         public bool PredictTheWinner(int[] nums)
         {
-            _firstPlayerLead = InitializeMatrix(nums);
-
-            return ComputeFirstPlayerLeads(nums, 0, nums.Length - 1) >= 0;
+            return IterativeSolution(nums);
         }
 
-        private int ComputeFirstPlayerLeads(int[] nums, int i, int j)
+        private bool IterativeSolution(int[] nums)
         {
-            if (i > j) return 0;
+            var n = nums.Length;
+            int[] lead = new int[n];
 
-            if (i == j) return nums[i];
-            if (i + 1 == j) return Math.Abs(nums[i] - nums[j]);
+            for (var i = n - 1; i >= 0; i--)
+            {
+                lead[i] = nums[i];
+                for (var j = i + 1; j < n; j++)
+                    lead[j] = Math.Max(nums[i] - lead[j], nums[j] - lead[j - 1]);
+            }
 
-            if (_firstPlayerLead[i,j] == int.MinValue)
-                _firstPlayerLead[i,j] = Math.Max(nums[i] - ComputeFirstPlayerLeads(nums, i + 1, j),
-                    nums[j] - ComputeFirstPlayerLeads(nums, i, j - 1));
-
-            return _firstPlayerLead[i, j];
+            return lead[n - 1] >= 0;
         }
 
-        private static int[,] InitializeMatrix(int[] nums)
-        {
-            var matrix = new int[nums.Length, nums.Length];
-            for(var i=0; i < nums.Length ; i++)
-                for (var j = 0; j < nums.Length; j++)
-                    matrix[i, j] = int.MinValue;
-
-            return matrix;
-        }
-        
     }
 }
