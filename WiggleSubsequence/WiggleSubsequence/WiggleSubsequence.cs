21c6f29e640d6537357d92cd36c45c243f7920e8
Rajat <rajatshuvro@gmail.com>
2017-11-29 19:37:54 -0800

    first commit

---
+++ WiggleSubsequence/WiggleSubsequence/WiggleSubsequence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WiggleSubsequence
+{
+    public class WiggleSubsequence
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int WiggleMaxLength(int[] nums)
+        {
+            if (nums.Length < 2) return nums.Length;
+
+
+            return 0;
+        }
+    }
+}
