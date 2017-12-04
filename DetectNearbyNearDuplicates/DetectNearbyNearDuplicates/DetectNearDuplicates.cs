c496f327b3ad9fc486cb7e742819c04d92090ac1
Rajat Roy <rroy1@illumina.com>
2017-12-03 16:00:17 -0800

    first pass solution

---
+++ DetectNearbyNearDuplicates/DetectNearbyNearDuplicates/DetectNearDuplicates.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DetectNearbyNearDuplicates
+{
+    class DetectNearDuplicates
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detecting if the array contains near duplicates nearby!");
+
+            if (!ContainsNearbyAlmostDuplicate(new int[] { }, 0, 0))
+            {
+                Console.Write("PASSED");
+                Console.WriteLine(" null test case");
+            }
+            else
+            {
+                Console.Write("FAILED");
+                Console.WriteLine(" null test case");
+                Console.ReadKey();
+                return;
+            }
+
+            if (ContainsNearbyAlmostDuplicate(new [] { 4,5}, 0, 0))
+            {
+                Console.Write("PASSED");
+                Console.WriteLine(" k=0, t=0 test case");
+            }
+            else
+            {
+                Console.Write("FAILED");
+                Console.WriteLine(" k=0, t=0 test case");
+                Console.ReadKey();
+                return;
+            }
+
+
+            if (ContainsNearbyAlmostDuplicate(new[] { 4, 4, 5 }, 1, 1))
+            {
+                Console.Write("PASSED");
+                Console.WriteLine(" k=1, t=1 test case");
+            }
+            else
+            {
+                Console.Write("FAILED");
+                Console.WriteLine(" [4,4,5], k=1, t=1 test case");
+                Console.ReadKey();
+                return;
+            }
+
+            if (! ContainsNearbyAlmostDuplicate(new[] { 4, 6, 9 }, 1, 1))
+            {
+                Console.Write("PASSED");
+                Console.WriteLine(" k=1, t=1 test case");
+            }
+            else
+            {
+                Console.Write("FAILED");
+                Console.WriteLine(" k=1, t=1 test case");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Test(int[] nums, int k, int t, bool expectedResult)
+        {
+
+        }
+
+        public static bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            if (nums == null || nums.Length == 0) return false;
+            if (k == 0 && t == 0) return true;
+
+            if (k < 1) return false;
+
+            var buckets = new HashSet<int>();
+
+            for (int i= -k-1, j=0; j < nums.Length; i++, j++)
+            {
+                if (i >= 0)
+                {
+                    var bucketNo = GetBucketNo(nums[i], t);
+                    buckets.Remove(bucketNo);
+                }
+
+                var newBucketNo = GetBucketNo(nums[j], t);
+                if (buckets.Contains(newBucketNo)) return true;
+                buckets.Add(newBucketNo);
+            }
+
+            return false;
+        }
+
+        private static int GetBucketNo(int x, int bucketSize)
+        {
+            return 2 * x / bucketSize;
+        }
+    }
+}
