06264c2d6b6ffb01a351064f2a3d638534fded84
Rajat Roy <rroy1@illumina.com>
2017-12-19 12:57:27 -0800

    adding unit tests. first test passing

--- LongestPalindromicSubsequence/LongestPalindromicSubsequence/LongestPalinSubLen.cs
+++ LongestPalindromicSubsequence/LongestPalindromicSubsequence/LongestPalinSubLen.cs
@@ -7,19 +7,41 @@ namespace LongestPalindromicSubsequence
         static void Main(string[] args)
         {
             Console.WriteLine("Finding length of longest palindromic subsequence!");
+
+            var result = UnitTest("bbbab", 4);
+
+            if (result)
+                Console.WriteLine("Passed all tests");
+
+            Console.ReadKey();
+        }
+
+        private static bool UnitTest(string s, int expedtedLen)
+        {
+            var sol = new LongestPalinSubLen();
+            var len = sol.LongestPalindromeSubseq(s);
+
+            if (len == expedtedLen)
+            {
+                Console.WriteLine($"Passed case: {s}");
+                return true;
+            }
+            Console.WriteLine($"Failed for: {s}. Expected {expedtedLen}, observed {len}");
+            return false;
         }
 
         private int[,] _dp;
 
         public int LongestPalindromeSubseq(string s)
         {
-            _dp = new int[2, s.Length];
+            _dp = new int[s.Length, s.Length];
 
             return MaxPalinSubLen(s, 0, s.Length - 1);
         }
 
         private int MaxPalinSubLen(string s, int i, int j)
         {
+            if (i < 0 || j < 0 || i >= s.Length || j >= s.Length || i > j) return 0;
             if (i == j) return 1;
 
             if (_dp[i, j] != 0) return _dp[i, j];
@@ -31,6 +53,6 @@ namespace LongestPalindromicSubsequence
 
             return _dp[i, j];
         }
-        
+
     }
 }
