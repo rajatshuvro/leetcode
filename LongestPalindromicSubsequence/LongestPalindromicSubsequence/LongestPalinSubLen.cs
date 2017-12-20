df33e3c13db1e2d5c520a1db2ba9b77b14c873cc
Rajat Roy <rroy1@illumina.com>
2017-12-20 09:58:27 -0800

    crating the iterative method

--- LongestPalindromicSubsequence/LongestPalindromicSubsequence/LongestPalinSubLen.cs
+++ LongestPalindromicSubsequence/LongestPalindromicSubsequence/LongestPalinSubLen.cs
@@ -32,11 +32,49 @@ namespace LongestPalindromicSubsequence
 
         private int[,] _dp;
 
+        //public int LongestPalindromeSubseq(string s)
+        //{
+        //    _dp = new int[s.Length, s.Length];
+
+        //    return MaxPalinSubLen(s, 0, s.Length - 1);
+        //}
+
+        //memory efficient O(n) iterative solution
+        //we will move along the diagonal of the nxn matrix. In each iteration, the difference between i and j will be constant
         public int LongestPalindromeSubseq(string s)
         {
-            _dp = new int[s.Length, s.Length];
+            var n = s.Length;
+            if (n == 0) return 0;
+            var scratches = GetScratches(n);
+            var scratchIndex = 1;
 
-            return MaxPalinSubLen(s, 0, s.Length - 1);
+            for (var k = 1; k < n; k++)
+            {
+                for (var i = 0; i < n - k; i++)
+                {
+                    var j = i + k;
+                    if (s[i] == s[j])
+                        scratches[1 - scratchIndex, j] = scratches[1 - scratchIndex, j] + 2; // dp[i][j]= dp[i+1][j-1]+2
+                    else
+                    {
+                        // dp[i,j]= max (dp[i+1, j], dp[i, j-1]
+                        scratches[1 - scratchIndex, j] =
+                            Math.Max(scratches[scratchIndex, j], scratches[scratchIndex, j + 1]);
+                    }
+                }
+                scratchIndex = 1 - scratchIndex;
+
+            }
+            return scratches[scratchIndex, n - 1];
+        }
+
+        
+        private static int[,] GetScratches(int n)
+        {
+            var scratches = new int[2, n];
+            for (var i = 0; i < n; i++)
+                scratches[0, i] = 1;// by definition, the diagonal elements are all 1, since palindrome of a subsequence of length 1 is 1.
+            return scratches;
         }
 
         private int MaxPalinSubLen(string s, int i, int j)
