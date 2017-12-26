ce279cc1456a1a274e49377c2b0362f44e1e90cb
Rajat <rajatshuvro@gmail.com>
2017-12-26 01:25:38 -0800

    O(1) solution thanks to mentioned discussion

---
+++ BulbSwitcher/BulbSwitcher/BulbSwitcher1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BulbSwitcher
+{
+    class BulbSwitcher1
+    {
+        //https://discuss.leetcode.com/topic/31929/math-solution
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bulb switcher 1!");
+        }
+
+        public int BulbSwitch(int n)
+        {
+            return (int) Math.Sqrt(n);
+        }
+    }
+}
