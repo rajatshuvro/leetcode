9fadf91720277458c3214ba33fc99807b8c9414d
Rajat <rajatshuvro@gmail.com>
2017-12-28 23:53:46 -0800

    minor modifications to output

--- RedundantConnection/RedundantConnection/RedundantEdge.cs
+++ RedundantConnection/RedundantConnection/RedundantEdge.cs
@@ -37,7 +37,7 @@ namespace RedundantConnection
             var sb = new StringBuilder();
             sb.Append('[');
             for (var i = 0; i < edges.GetLength(0); i++)
-                sb.Append($"[{edges[i, 0]}, {edges[i, 1]}],");
+                sb.Append($"[{edges[i, 0]}, {edges[i, 1]}]");
             sb.Append(']');
 
             return sb.ToString();
@@ -45,7 +45,7 @@ namespace RedundantConnection
 
         public static int[] FindRedundantConnection(int[,] edges)
         {
-
+            return new[] {0, 0};
         }
     }
 }
