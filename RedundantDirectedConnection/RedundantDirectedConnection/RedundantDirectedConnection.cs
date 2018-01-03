c53fa6d5d8a7c0827228c19e7b0db0c2d785e5ff
Rajat Roy <rroy1@illumina.com>
2018-01-03 13:57:17 -0800

    don't agree with the failed test case

--- RedundantDirectedConnection/RedundantDirectedConnection/RedundantDirectedConnection.cs
+++ RedundantDirectedConnection/RedundantDirectedConnection/RedundantDirectedConnection.cs
@@ -14,6 +14,7 @@ namespace RedundantDirectedConnection
             var result = UnitTest(new[,] { { 1, 2 }, { 1, 3 }, { 2, 3 } }, new[] { 2, 3 });
             result &= UnitTest(new[,] { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 1 }, { 1, 5 } }, new[] { 4, 1 });
             result &= UnitTest(new[,] { { 3, 4 }, { 1, 2 }, { 2, 3 }, { 3, 5 }, { 5, 2 } }, new[] { 5, 2 });
+            result &= UnitTest(new [,] {{2, 1},{3, 1},{4, 2},{1, 4}}, new[] {2, 1});
             if (result)
                 Console.WriteLine("Passed all tests");
 
@@ -33,9 +34,17 @@ namespace RedundantDirectedConnection
             return false;
         }
 
-        private static List<int> FindRedundantConnection(int[,] edges)
+        public static int[] FindRedundantConnection(int[,] edges)
         {
-            throw new NotImplementedException();
+            var unionFinder = new UnionFinder<int>();
+            int a = -1, b = -1;
+            for (var i = 0; i < edges.GetLength(0); i++)
+            {
+                if (!unionFinder.Unite(edges[i, 0], edges[i, 1])) continue;
+                a = edges[i, 0];
+                b = edges[i, 1];
+            }
+            return new[] { a, b };
         }
 
         private static string PrintEdges(int[,] edges)
@@ -47,6 +56,56 @@ namespace RedundantDirectedConnection
             sb.Append(']');
 
             return sb.ToString();
+        }
+
+        class UnionFinder<T> where T : IEquatable<T>, IComparable<T>
+        {
+            private readonly Dictionary<T, T> _founder;
+            private readonly Dictionary<T, List<T>> _clans;
+
+            public UnionFinder()
+            {
+                _founder = new Dictionary<T, T>();
+                _clans = new Dictionary<T, List<T>>();
+            }
+            // returns true if a and b both belong to the same clan
+            public bool Unite(T a, T b)
+            {
+                // both items are present, we need to merge two clans if different
+                if (_founder.ContainsKey(a) && _founder.ContainsKey(b))
+                {
+                    if (_founder[a].Equals(_founder[b]))
+                        return true;
+
+                    var founderB = _founder[b];
+                    foreach (var member in _clans[founderB])
+                    {
+                        _founder[member] = _founder[a];
+                    }
+                    _clans[_founder[a]].AddRange(_clans[founderB]);
+                    _clans.Remove(founderB);
+                    return false;
+                }
+                // at least one item is present
+                if (_founder.ContainsKey(a))
+                {
+                    _founder[b] = _founder[a];
+                    _clans[_founder[a]].Add(b);
+                    return false;
+                }
+                if (_founder.ContainsKey(b))
+                {
+                    _founder[a] = _founder[b];
+                    _clans[_founder[b]].Add(a);
+                    // we are assigning two parents to a node, that is an error
+                    return true;
+                }
+                // no items present
+                _founder[a] = a;
+                _founder[b] = a;
+                _clans.Add(a, new List<T> { a, b });
+                return false;
+            }
         }
 
     }
