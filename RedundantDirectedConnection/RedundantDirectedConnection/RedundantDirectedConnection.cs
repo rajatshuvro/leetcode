92c245ea372804ee87142642a9d4a905a75d1749
Rajat Roy <rroy1@illumina.com>
2018-01-15 17:02:01 -0800

    some progress with the graph

--- RedundantDirectedConnection/RedundantDirectedConnection/RedundantDirectedConnection.cs
+++ RedundantDirectedConnection/RedundantDirectedConnection/RedundantDirectedConnection.cs
@@ -62,44 +62,66 @@ namespace RedundantDirectedConnection
 
     }
 
-    internal class DirectedGraphBuilder<T> where T:IEquatable<T>
+    internal class DirectedGraphBuilder
     {
-        private Dictionary<T, GraphNode<T>> _vertices;
+        private readonly Dictionary<int, GraphNode> _vertices;
+        private readonly List<Tuple<int, int>> _candidates;
         public DirectedGraphBuilder()
         {
-            _vertices = new Dictionary<T, GraphNode<T>>();
+            _vertices = new Dictionary<int, GraphNode>();
+            _candidates = new List<Tuple<int, int>>();
         }
 
-        public bool AddEdge(T a, T b)
+        public void AddEdges(int[,] edges)
         {
-            if (_vertices.ContainsKey(b) && _vertices[b].Parent != null) return false;
-            if (!_vertices.ContainsKey(a)) 
-                _vertices[a]= new GraphNode<T>(b);
-            _vertices[a].AddChild(b);
-            return true;
-        }
-
-        private class GraphNode<T> where T:IEquatable<T>
-        {
-            public readonly T Parent;
-            private List<T> _children;
-
-            public GraphNode(T parent)
+            for (var i = 0; i < edges.GetLength(0); i++)
             {
-                Parent = parent;
+                AddEdge(edges[i, 0], edges[i, 1]);
+            }
+        }
+
+        public void AddEdge(int a, int b)
+        {
+            if (!_vertices.ContainsKey(a)) 
+                _vertices[a]= new GraphNode(a);
+            if (!_vertices.ContainsKey(b))
+                _vertices[b] = new GraphNode(b);
+
+            _vertices[a].AddChild(b);
+            if (_vertices[b].AddParent(a)) return ;
+
+            _candidates.Add(Tuple.Create(a,b));
+            _candidates.Add(Tuple.Create(_vertices[b].Parent, b));
+        }
+
+        private class GraphNode
+        {
+            public readonly int Label;
+            public int Parent;
+            private List<int> _children;
+
+
+            public GraphNode(int label)
+            {
+                Label = label;
             }
 
-            public void AddChild(T child)
+            public void AddChild(int child)
             {
-                if (_children==null) _children= new List<T>();
+                if (_children==null) _children= new List<int>();
 
                 _children.Add(child);
             }
 
-            public bool IsParentOf(T child)
+            public bool AddParent(int parent)
             {
-                return _children != null && _children.Contains(child);
+                // parent stays unchanged if this node already had a parent
+                if (Parent != 0) return false;
+                Parent = parent;
+                return true;
+
             }
+            
         }
     }
 }
