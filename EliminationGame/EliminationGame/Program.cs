56b4c851ef59004d644a682e728a17d0a71a8ddc
Rajat Roy <rroy1@illumina.com>
2018-02-22 12:01:39 -0800

    solution skeleton

---
+++ EliminationGame/EliminationGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EliminationGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let the eliminatioin begin!");
+        }
+    }
+
+    public class Solution
+    {
+        public int LastRemaining(int n)
+        {
+            return -1;
+        }
+    }
+}
