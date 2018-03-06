f867bac6af69771af93f0ee1c37d8cb8d483886c
Rajat Roy <rroy1@illumina.com>
2018-03-05 20:37:37 -0800

    new problem

---
+++ PyramidTransitionMatrix/PyramidBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PyramidTransitionMatrix
+{
+    class PyramidBuilder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Building pyramid with transition matrices!");
+        }
+
+        public bool PyramidTransition(string bottom, IList<string> allowed)
+        {
+            return GetNextFloors(bottom, allowed).Any(nextFloor => PyramidTransition(nextFloor, allowed));
+        }
+
+        private IEnumerable<string> GetNextFloors(string bottom, IList<string> allowed)
+        {
+            var floorList = new List<string>();
+            for (var i = 0; i < bottom.Length - 1; i++)
+            {
+
+            }
+        }
+    }
+}
