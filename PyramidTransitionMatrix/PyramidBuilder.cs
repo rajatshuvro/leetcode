694f609794b094cbbe13343ae913143b1d8b2687
Rajat <rajatshuvro@gmail.com>
2018-03-06 23:22:04 -0800

    nice clean solution. But no percentile data available

--- PyramidTransitionMatrix/PyramidBuilder.cs
+++ PyramidTransitionMatrix/PyramidBuilder.cs
@@ -10,20 +10,77 @@ namespace PyramidTransitionMatrix
         static void Main(string[] args)
         {
             Console.WriteLine("Building pyramid with transition matrices!");
+
+            UnitTest("XYZ", new List<string>(){ "XYD", "YZE", "DEA", "FFF" }, true);
+            UnitTest("CCC", new List<string>() { "CBB", "ACB", "ABD", "CDB", "BDC", "CBC", "DBA", "DBB", "CAB", "BCB", "BCC", "BAA", "CCD", "BDD", "DDD", "CCA", "CAA", "CCC", "CCB" }, true);
+            
         }
+
+        private static void UnitTest(string bottom, List<string> allowed, bool expectedResult)
+        {
+            var pyBuilder = new PyramidBuilder();
+
+            var result = pyBuilder.PyramidTransition(bottom, allowed);
+
+            Console.Write($"Testing : {bottom} with {string.Join(',', allowed)}.....");
+            Console.WriteLine(result == expectedResult ? "Passed" : "FAILED");
+        }
+
+        HashSet<string> _validBottoms;
+        private Dictionary<string, List<char>> _allowedLookup;
 
         public bool PyramidTransition(string bottom, IList<string> allowed)
         {
-            return GetNextFloors(bottom, allowed).Any(nextFloor => PyramidTransition(nextFloor, allowed));
+            //generate 2-d lookup for allowed
+            _allowedLookup = new Dictionary<string, List<char>>();
+            foreach (var tripple in allowed)
+            {
+                var key = tripple.Substring(0, 2);
+                if (!_allowedLookup.ContainsKey(key))
+                    _allowedLookup[key] = new List<char>(){tripple[2]};
+                else _allowedLookup[key].Add(tripple[2]);
+            }
+            _validBottoms = new HashSet<string>();
+            return PyramidTransition(bottom);
         }
 
-        private IEnumerable<string> GetNextFloors(string bottom, IList<string> allowed)
+        private bool PyramidTransition(string bottom)
         {
-            var floorList = new List<string>();
+            if (bottom.Length == 2) return _allowedLookup.ContainsKey(bottom);
+            if (_validBottoms.Contains(bottom)) return true;//using memoization
+
+            foreach (var nextFloor in GetNextFloors(bottom))
+            {
+                if (!PyramidTransition(nextFloor)) continue;
+
+                _validBottoms.Add(bottom);
+                return true;
+            }
+
+            return false;
+        }
+
+        private IEnumerable<string> GetNextFloors(string bottom)
+        {
+            var floorList = new List<string>(){""};
+            
             for (var i = 0; i < bottom.Length - 1; i++)
             {
+                var pair = bottom.Substring(i, 2);
+                if (_allowedLookup.ContainsKey(pair))
+                {
+                    var newFloors = new List<string>();
+                    foreach (var c in _allowedLookup[pair])
+                    {
+                        newFloors.AddRange(floorList.Select(f => f + c.ToString()));
+                    }
 
+                    floorList = newFloors;
+                }
+                else return new List<string>();//returning empty string list
             }
+
+            return floorList;
         }
     }
 }
