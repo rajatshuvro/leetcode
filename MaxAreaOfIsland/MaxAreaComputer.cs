7c221ce76927e5fc2e626b992db07a47306b60ba
Rajat Roy <rroy1@illumina.com>
2018-09-06 11:49:15 -0700

    making a solution object

--- MaxAreaOfIsland/MaxAreaComputer.cs
+++ MaxAreaOfIsland/MaxAreaComputer.cs
@@ -28,7 +28,8 @@ namespace MaxAreaOfIsland
                 {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
 
-            if (MaxAreaOfIsland(grid) == 11)
+            var s = new Solution();
+            if (s.MaxAreaOfIsland(grid) == 11)
                 return true;
 
             Console.WriteLine("Failed unit test 2");
@@ -46,47 +47,14 @@ namespace MaxAreaOfIsland
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
 
-            if (MaxAreaOfIsland(grid) == 6)
+            var s = new Solution();
+            if (s.MaxAreaOfIsland(grid) == 6)
                 return true;
 
             Console.WriteLine("Failed unit test 1");
             return false;
         }
 
-        public static int MaxAreaOfIsland(int[,] grid)
-        {
-            var newIslandId = -1;
-            var maxArea = 0;
-            for (int i = 0; i < grid.GetLength(0); i++)
-                for (int j = 0; j < grid.GetLength(1); j++)
-                {
-                    if (grid[i, j] <= 0) continue;
-
-                    var area = ExploreNewIsland(grid, i, j, newIslandId);
-
-                    newIslandId--;
-
-                    if (area > maxArea) maxArea = area;
-                }
-
-            return maxArea;
-        }
-
-        private static int ExploreNewIsland(int[,] grid, int i, int j, int islandId)
-        {
-            if (i < 0 || i >= grid.GetLength(0)) return 0;
-            if (j < 0 || j >= grid.GetLength(1)) return 0;
-            if (grid[i, j] <= 0) return 0;
-
-            int size = 1;
-            grid[i, j] = islandId;
-            
-            size += ExploreNewIsland(grid, i - 1, j, islandId);
-            size += ExploreNewIsland(grid, i + 1, j, islandId);
-            size += ExploreNewIsland(grid, i, j - 1, islandId);
-            size += ExploreNewIsland(grid, i, j + 1, islandId);
-
-            return size;
-        }
+        
     }
 }
---
+++ MaxAreaOfIsland/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace MaxAreaOfIsland
+{
+    public class Solution
+    {
+        private int[,] _grid;
+        private int _dim1;
+        private int _dim2;
+        public int MaxAreaOfIsland(int[,] grid)
+        {
+            var newIslandId = -1;
+            var maxArea = 0;
+            _grid = grid;
+            _dim1 = grid.GetLength(0);
+            _dim2 = grid.GetLength(1);
+            for (int i = 0; i < _dim1; i++)
+            for (int j = 0; j < _dim2; j++)
+            {
+                if (grid[i, j] <= 0) continue;
+
+                var area = ExploreNewIsland(i, j, newIslandId);
+
+                newIslandId--;
+
+                if (area > maxArea) maxArea = area;
+            }
+            return maxArea;
+        }
+
+        private int ExploreNewIsland(int i, int j, int islandId)
+        {
+            if (i < 0 || i >= _dim1) return 0;
+            if (j < 0 || j >= _dim2) return 0;
+            if (_grid[i, j] <= 0) return 0;
+
+            int size = 1;
+            _grid[i, j] = islandId;
+
+            size += ExploreNewIsland(i - 1, j, islandId);
+            size += ExploreNewIsland(i + 1, j, islandId);
+            size += ExploreNewIsland(i, j - 1, islandId);
+            size += ExploreNewIsland(i, j + 1, islandId);
+
+            return size;
+        }
+    }
+}
