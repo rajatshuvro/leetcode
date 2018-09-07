195252e88dd265a7e2853dab6da95a8da8096104
Rajat Roy <rroy1@illumina.com>
2018-09-07 15:19:30 -0700

    Reordered power of two problem

---
+++ ReorderedPowerOfTwo/ReorderedPowerOfTwo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ReorderedPowerOfTwo
+{
+    class ReorderedPowerOfTwo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reordered power of two!");
+
+            var result = UnitTest1();
+            result &= UnitTest2();
+
+            if (result)
+                Console.WriteLine("Passed all unit tests");
+
+            Console.Read();
+        }
+
+        private static bool UnitTest2()
+        {
+            var s = new Solution();
+            if (s.ReorderedPowerOf2(46)) return true;
+
+            Console.WriteLine("Failed unit test 2");
+            return false;
+        }
+
+        private static bool UnitTest1()
+        {
+            var s = new Solution();
+            if (s.ReorderedPowerOf2(16)) return true;
+
+            Console.WriteLine("Failed unit test 1");
+            return false;
+        }
+
+        private static bool UnitTest3()
+        {
+            var s = new Solution();
+            if (!s.ReorderedPowerOf2(24)) return true;
+
+            Console.WriteLine("Failed unit test 3");
+            return false;
+        }
+    }
+}
---
+++ ReorderedPowerOfTwo/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+
+namespace ReorderedPowerOfTwo
+{
+    public class Solution
+    {
+        public bool ReorderedPowerOf2(int N)
+        {
+            var profileN = GetIntegerProfile(N);
+            var i = GetMinNumberForProfile(profileN);
+
+            for (; i <= 1000000000; i <<= 1)
+            {
+                var profileI = GetIntegerProfile(i);
+                if (profileN.SequenceEqual(profileI)) return true;
+            }
+
+            return false;
+        }
+
+        private int GetMinNumberForProfile(int[] profile)
+        {
+            var digitCount = profile.Sum();
+            switch (digitCount)
+            {
+                case 1:
+                    return 1;
+                case 2:
+                    return 16;
+                case 3:
+                    return 128;
+                case 4:
+                    return 1_024;
+                case 5:
+                    return 16_384;
+                case 6:
+                    return 131_072;
+                case 7:
+                    return 1_048_576;
+                case 8:
+                    return 16_777_216;
+                default:
+                    return 134_217_728;
+            }
+         
+        }
+
+        private int[] GetIntegerProfile(int n)
+        {
+            int[] profile = new int[10];
+            while (n != 0)
+            {
+                var lastDigit = n % 10;
+                n /= 10;
+                profile[lastDigit]++;
+            }
+
+            return profile;
+        }
+    }
+}
