c5aaf9d4bc7fe8846d8b7e340770bf6c10a083ff
Rajat Roy <rroy1@illumina.com>
2018-09-10 08:54:17 -0700

    starting the BST iterator

---
+++ BSTIterator/BSTIterator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace BSTIterator
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    //https://leetcode.com/problems/binary-search-tree-iterator/description/
+    public class BSTIterator
+    {
+        private List<TreeNode> _parentNodes= new List<TreeNode>();
+        private TreeNode _currentNode;
+        public BSTIterator(TreeNode root)
+        {
+            _currentNode = root;
+        }
+
+        /** @return whether we have a next smallest number */
+        public bool HasNext()
+        {
+
+        }
+
+        /** @return the next smallest number */
+        public int Next()
+        {
+            while (!IsLeaf(_currentNode))
+            {
+                _parentNodes.Add(_currentNode);
+
+            }
+        }
+
+        private bool IsLeaf(TreeNode node)
+        {
+            return node.left == null && node.right == null;
+        }
+    }
+}
---
+++ BSTIterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSTIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's iterate through a binary search tree!");
+        }
+    }
+}
