8c3fe5ebf638b5d55fc92c1b7e8df879d1e0e763
Rajat Roy <rroy1@illumina.com>
2018-09-10 17:09:12 -0700

    work in progress for BST iterator

--- BSTIterator/BSTIterator.cs
+++ BSTIterator/BSTIterator.cs
@@ -13,11 +13,26 @@ namespace BSTIterator
     //https://leetcode.com/problems/binary-search-tree-iterator/description/
     public class BSTIterator
     {
-        private List<TreeNode> _parentNodes= new List<TreeNode>();
+        private List<TreeNode> _ancestors= new List<TreeNode>();
+        // node status can be
+        // ------left-traversed (l) 
+        // ------right-traversed (r) 
+        private Dictionary<TreeNode, char> _nodeStatus = new Dictionary<TreeNode, char>();
         private TreeNode _currentNode;
         public BSTIterator(TreeNode root)
         {
             _currentNode = root;
+            DiveLeft();
+        }
+
+        private void DiveLeft()
+        {
+            while (_currentNode.left != null)
+            {
+                _nodeStatus[_currentNode] = 'l';
+                _ancestors.Add(_currentNode);
+                _currentNode = _currentNode.left;
+            }
         }
 
         /** @return whether we have a next smallest number */
@@ -29,11 +44,31 @@ namespace BSTIterator
         /** @return the next smallest number */
         public int Next()
         {
-            while (!IsLeaf(_currentNode))
-            {
-                _parentNodes.Add(_currentNode);
+            if (_currentNode == null) return int.MinValue;
 
+            var value = _currentNode.val;
+
+            //move to next node
+            if (_currentNode.right != null && _nodeStatus[_currentNode]=='l')
+            {
+                _ancestors.Add(_currentNode);
+                _nodeStatus[_currentNode] = 'r';
+                //var rightNode = _currentNode.right;
+                _currentNode = _currentNode.right;
+
+                DiveLeft();
             }
+            else
+            {
+                while (_currentNode != null)
+                {
+                }
+
+                _currentNode = _ancestors.Count > 0 ? _ancestors[_ancestors.Count - 1] : null;
+                _ancestors.RemoveAt(_ancestors.Count);
+            }
+
+            return value;
         }
 
         private bool IsLeaf(TreeNode node)
