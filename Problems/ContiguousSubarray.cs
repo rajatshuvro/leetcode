baf6b37b3bc9113310bc153845cac331b92fb9c6
Rajat Roy <rroy1@illumina.com>
2018-09-30 00:24:24 -0700

    contiguous subarray problem

---
+++ Problems/ContiguousSubarray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class ContiguousSubarray
+    {
+        //https://leetcode.com/problems/contiguous-array/description/
+        //Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+        public int FindMaxLength(int[] nums)
+        {
+            var firstIndexOfSum = new Dictionary<int, int>();
+            firstIndexOfSum[0] = -1;
+
+            var sum = 0;
+            var maxLength = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) sum--;
+                else sum++;
+
+                if (firstIndexOfSum.TryGetValue(sum, out var index))
+                {
+                    if( maxLength < i - index) maxLength = i-index ;
+                }
+                else firstIndexOfSum[sum] = i;
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ UnitTests/ContiguousSubarrayTests.cs
@@ -0,0 +1,54 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ContiguousSubarrayTests
+    {
+        [Fact]
+        public void OneElementTest()
+        {
+            var array = new ContiguousSubarray();
+
+            Assert.Equal(0, array.FindMaxLength(new[] { 1 }));
+        }
+        [Fact]
+        public void TwoElementTest()
+        {
+            var array = new ContiguousSubarray();
+
+            Assert.Equal(2, array.FindMaxLength(new []{0,1}));
+            Assert.Equal(0, array.FindMaxLength(new[] { 0, 0 }));
+        }
+
+        [Fact]
+        public void ThreeElementTest()
+        {
+            var array = new ContiguousSubarray();
+
+            Assert.Equal(2, array.FindMaxLength(new[] { 0, 1, 0 }));
+            Assert.Equal(2, array.FindMaxLength(new[] { 1, 1, 0 }));
+            Assert.Equal(0, array.FindMaxLength(new[] { 1, 1, 1 }));
+        }
+
+        [Fact]
+        public void FourElementTest()
+        {
+            var array = new ContiguousSubarray();
+
+            Assert.Equal(4, array.FindMaxLength(new[] { 0, 1, 1 ,0}));
+            Assert.Equal(2, array.FindMaxLength(new[] { 0, 1, 1, 1 }));
+            Assert.Equal(2, array.FindMaxLength(new[] { 0, 0, 1, 0 }));
+        }
+
+        [Fact]
+        public void ManyElementTest()
+        {
+            var array = new ContiguousSubarray();
+
+            Assert.Equal(8, array.FindMaxLength(new[] { 0, 1, 1, 0, 1, 1, 0, 0 }));
+            Assert.Equal(6, array.FindMaxLength(new[] { 0, 1, 1, 1, 0, 0 }));
+            Assert.Equal(4, array.FindMaxLength(new[] { 0, 0, 1, 0 , 0,1}));
+        }
+    }
+}
