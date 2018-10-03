409555c5e08be6430cb3faaab030c7ab9fad51d5
Rajat Roy <rroy1@illumina.com>
2018-10-02 23:08:46 -0700

    Slow but accepted solution to hard problem with simple algorithm.

---
+++ Problems/LFUCache.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using DataStructures;
+
+namespace Problems
+{
+    public class CacheItem:IComparable<CacheItem>
+    {
+        public readonly int Key;
+        private int _value;
+        private int _frequency;
+        private int _accessTime;
+
+        public CacheItem(int key, int value, int time)
+        {
+            Key = key;
+            _value = value;
+            _accessTime = time;
+            _frequency = 1;
+        }
+
+        public int GetValue(int time)
+        {
+            _frequency++;
+            _accessTime = time;
+            return _value;
+        }
+
+        public int CompareTo(CacheItem other)
+        {
+            var comparison = _frequency.CompareTo(other._frequency);
+            return comparison != 0 ? comparison : _accessTime.CompareTo(other._accessTime);
+        }
+
+        public void UpdateValue(int value, int time)
+        {
+            _value = value;
+            _frequency++;
+            _accessTime = time;
+        }
+    }
+
+    public class LFUCache
+    {
+        private readonly int _capacity;
+        private readonly Dictionary<int, CacheItem> _items;
+
+        private int _time;
+        public LFUCache(int capacity)
+        {
+            _capacity = capacity;
+            _time = 0;
+            _items = new Dictionary<int, CacheItem>(capacity);
+        }
+
+        public int Get(int key)
+        {
+            if (_items.TryGetValue(key, out var item))
+            {
+                _time++;
+                return item.GetValue(_time);
+            }
+            return -1;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (_capacity == 0) return;
+            _time++;
+
+            if (_items.ContainsKey(key))
+            {
+                _items[key].UpdateValue(value, _time);
+                return;
+            }
+
+            if (_capacity <= _items.Count)
+            {
+                var item = _items.Values.Min();
+                _items.Remove(item.Key);
+            }
+
+            _items.Add(key, new CacheItem(key, value, _time));
+
+        }
+    }
+}
---
+++ UnitTests/LfuCacheTests.cs
@@ -0,0 +1,51 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class LfuCacheTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            var cache = new LFUCache(2);
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            Assert.Equal(1,cache.Get(1));     
+
+            cache.Put(3, 3);    // evicts key 2
+            Assert.Equal(-1, cache.Get(2));       // returns -1 (not found)
+            
+            Assert.Equal(3, cache.Get(3));       // returns 3.
+            cache.Put(4, 4);    // evicts key 1.
+            Assert.Equal(-1, cache.Get(1));       // returns -1 (not found)
+            Assert.Equal(3, cache.Get(3));       // returns 3
+            Assert.Equal(4, cache.Get(4));       // returns 4
+        }
+
+        [Fact]
+        public void UpdateElement()
+        {
+            var cache = new LFUCache(2);
+            cache.Put(3, 1);
+            cache.Put(2, 1);
+            cache.Put(2, 2);
+            cache.Put(4, 4);
+            Assert.Equal(2, cache.Get(2));
+        }
+        [Fact]
+        public void ShouldNotEvict()
+        {
+            var cache = new LFUCache(2);
+            Assert.Equal(-1, cache.Get(2));
+            cache.Put(2, 6);
+            Assert.Equal(-1, cache.Get(1));
+            cache.Put(1, 5);
+            cache.Put(1, 2);
+            Assert.Equal(2, cache.Get(1));
+            Assert.Equal(6, cache.Get(2));
+        }
+
+        
+    }
+}
