416d260ae86a722d91954c803f4c4619c8760425
Rajat Roy <rroy1@illumina.com>
2018-10-03 23:18:35 -0700

    creating doubly linked list to improve LRU and LFU cache

---
+++ DataStructures/DataStructures/DoublyLinkedList.cs
@@ -0,0 +1,101 @@
+﻿namespace DataStructures
+{
+    public class DoublyLinkedItem<T>
+    {
+        public T Value;
+        public DoublyLinkedItem<T> Left;
+        public DoublyLinkedItem<T> Right;
+
+        public DoublyLinkedItem(T value, DoublyLinkedItem<T> left, DoublyLinkedItem<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+
+    public class DoublyLinkedList<T> 
+    {
+        private DoublyLinkedItem<T> _leftMostItem;
+        private DoublyLinkedItem<T> _rightMostItem;
+
+        public void AddAtLeftEnd(DoublyLinkedItem<T> item)
+        {
+            if (_leftMostItem == null)
+            {
+                _leftMostItem = item;
+                _rightMostItem = item;
+                return;
+            }
+
+            item.Right = _leftMostItem;
+            _leftMostItem = item;
+        }
+
+        public void AddAtRightEnd(DoublyLinkedItem<T> item)
+        {
+            if (_rightMostItem == null)
+            {
+                _leftMostItem = item;
+                _rightMostItem = item;
+                return;
+            }
+
+            item.Left = _rightMostItem;
+            _rightMostItem = item;
+        }
+
+        public DoublyLinkedItem<T> RemoveFromLeftEnd()
+        {
+            if (_leftMostItem == null) return null;
+
+            var item = _leftMostItem;
+            _leftMostItem = _leftMostItem.Right;
+            if (_leftMostItem == null) _rightMostItem = null;
+            return item;
+        }
+
+        public DoublyLinkedItem<T> RemoveFromRightEnd()
+        {
+            if (_rightMostItem== null) return null;
+
+            var item = _rightMostItem;
+            _rightMostItem = _rightMostItem.Left;
+            if (_rightMostItem == null) _leftMostItem = null;
+            return item;
+        }
+
+        public void MoveRight(DoublyLinkedItem<T> item)
+        {
+            if (item.Right == null) return;
+
+            var right = item.Right;
+            var left = item.Left;
+            item.Right = right.Right;
+            item.Left = right;
+
+            right.Right = item;
+            right.Left = left;
+
+            if (left!=null) left.Right = right;
+            if (item.Right == null) _rightMostItem = item;
+        }
+
+        public void MoveLeft(DoublyLinkedItem<T> item)
+        {
+            if (item.Left == null) return;
+
+            var right = item.Right;
+            var left = item.Left;
+            item.Right = left;
+            item.Left = left.Left;
+
+            left.Right = right;
+            right.Left = item;
+
+            if (right != null) right.Left = left;
+            if (item.Left == null) _leftMostItem = item;
+        }
+
+    }
+}
