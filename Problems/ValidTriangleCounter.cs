a0e33f9ee5d57d383e81e19842040d2bdcab73d6
Rajat <rajatshuvro@gmail.com>
2018-10-12 00:11:22 -0700

    valid triangle count problem

---
+++ Problems/ValidTriangleCounter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problems
+{
+    //https://leetcode.com/problems/valid-triangle-number/description/
+    public class ValidTriangleCounter
+    {
+        private int[] _nums;
+        // this is a variant of the 3 sum problem where you are counting the number of trios that sum up in a certain manner
+        public int TriangleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+
+            _nums = nums;
+            var numValidTrios = 0;
+            for (int i = 2; i < nums.Length; i++)
+            {
+                numValidTrios += TriangleNumber(i);
+            }
+
+            return numValidTrios;
+        }
+        //find all valid trios where _num[i] is the largest
+        private int TriangleNumber(int i)
+        {
+            var count = 0;
+            var side3 = _nums[i];
+
+            for (int k = i - 1, j=0; k > j; k--)
+            {
+                var side2 = _nums[k];
+                //minimum required length of side1 so that the 3 sides make a triangle
+                var minSide1 = side3 - side2 + 1;
+
+                j = Array.BinarySearch(_nums, j, k-j, minSide1);
+                if (j < 0) j = ~j;
+                count += k - j;
+
+            }
+            return count;
+        }
+    }
+}
---
+++ UnitTests/ValidTriosTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ValidTriosTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            var triangleCounter = new ValidTriangleCounter();
+
+            Assert.Equal(3, triangleCounter.TriangleNumber(new []{ 2, 2, 3, 4 }));
+        }
+
+        [Fact]
+        public void Failed_case_1()
+        {
+            var triangleCounter = new ValidTriangleCounter();
+
+            Assert.Equal(7, triangleCounter.TriangleNumber(new[] { 1, 2, 3, 4, 5, 6}));
+        }
+
+        [Fact]
+        public void Failed_case_2()
+        {
+            var triangleCounter = new ValidTriangleCounter();
+
+            Assert.Equal(14, triangleCounter.TriangleNumber(new[] {58, 97, 2, 8, 2, 55, 14, 54, 2 }));
+        }
+
+        [Fact]
+        public void BinSearchTest()
+        {
+            var nums = new int[] {1,2,2,2,3,3,45,56,66 };
+            var j = Array.BinarySearch(nums, 2, 4, 40);
+            if (j < 0) j = ~j;
+
+            Assert.Equal(6, j);
+        }
+    }
+}
