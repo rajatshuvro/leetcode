e97ffd38effa6bf5ddb23c1fe930496f18229148
Rajat Roy <rroy1@illumina.com>
2018-10-13 22:58:45 -0700

    iterative inorder traversal done

---
+++ Problems/BinaryTreeTraversal.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using DataStructures;
+
+namespace Problems
+{
+    public class BinaryTreeTraversal
+    {
+        //https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+
+            var stack = new List<TreeNode>(){root};
+            var inOrder = new List<int>();
+
+            while (stack.Count>0)
+            {
+                var node = stack[stack.Count - 1];
+                if (node.left != null)
+                {
+                    stack.Add(node.left);
+                    node.left = null;
+                    continue;
+                }
+
+                stack.RemoveAt(stack.Count - 1);
+
+                inOrder.Add(node.val);
+                if (node.right != null)
+                {
+                    stack.Add(node.right);
+                    node.right = null;
+                }
+            }
+
+            return inOrder;
+        }
+
+        //https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            if (root == null) return new List<int>();
+
+            var stack = new List<TreeNode>() { root };
+            var preOrder = new List<int>();
+
+            while (stack.Count > 0)
+            {
+                var node = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                preOrder.Add(node.val);
+
+                if (node.left != null)
+                {
+                    stack.Add(node.left);
+                    node.left = null;
+                    continue;
+                }
+
+                if (node.right != null)
+                {
+                    stack.Add(node.right);
+                    node.right = null;
+                }
+            }
+
+            return preOrder;
+        }
+    }
+}
---
+++ UnitTests/IterativeInorderTests.cs
@@ -0,0 +1,73 @@
+﻿using DataStructures;
+using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class IterativeInorderTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            var node1 = new TreeNode(1);
+            var node2 = new TreeNode(2);
+            var node3 = new TreeNode(3);
+
+            node1.right = node2;
+            node2.left = node3;
+
+            var traverser = new BinaryTreeTraversal();
+            var inorderList = traverser.InorderTraversal(node1);
+
+            Assert.Equal(new []{1,3,2}, inorderList);
+        }
+
+        [Fact]
+        public void Balanced()
+        {
+            var node1 = new TreeNode(1);
+            var node2 = new TreeNode(2);
+            var node3 = new TreeNode(3);
+
+            node2.left = node1;
+            node2.right = node3;
+
+            var traverser = new BinaryTreeTraversal();
+            var inorderList = traverser.InorderTraversal(node2);
+
+            Assert.Equal(new[] { 1, 2, 3 }, inorderList);
+        }
+
+        [Fact]
+        public void Left_chain()
+        {
+            var node1 = new TreeNode(1);
+            var node2 = new TreeNode(2);
+            var node3 = new TreeNode(3);
+
+            node2.left = node1;
+            node3.left = node2;
+
+            var traverser = new BinaryTreeTraversal();
+            var inorderList = traverser.InorderTraversal(node3);
+
+            Assert.Equal(new[] { 1, 2, 3 }, inorderList);
+        }
+
+        [Fact]
+        public void Right_chain()
+        {
+            var node1 = new TreeNode(1);
+            var node2 = new TreeNode(2);
+            var node3 = new TreeNode(3);
+
+            node1.right = node2;
+            node2.right = node3;
+
+            var traverser = new BinaryTreeTraversal();
+            var inorderList = traverser.InorderTraversal(node1);
+
+            Assert.Equal(new[] { 1, 2, 3 }, inorderList);
+        }
+    }
+}
