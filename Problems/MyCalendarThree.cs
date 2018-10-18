57c95fe9a1c49adc10ecdc4e104b618d5bb0674b
Rajat Roy <rroy1@illumina.com>
2018-10-17 22:24:26 -0700

    calendar 3 problem

---
+++ Problems/MyCalendarThree.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using DataStructures;
+
+namespace Problems
+{
+    public class MyCalendarThree
+    {
+        private readonly IntervalTree _tree;
+        private int _maxOverlap = 0;
+        public MyCalendarThree()
+        {
+            _tree = new IntervalTree();
+        }
+
+        public int Book(int start, int end)
+        {
+            var interval = new Interval(start, end);
+
+            var overlapCount = _tree.GetOverlappingIntervals(interval).Count();
+
+            _tree.Add(interval);
+
+            if (_maxOverlap < overlapCount + 1) _maxOverlap = overlapCount + 1;
+            return _maxOverlap;
+        }
+    }
+}
---
+++ UnitTests/CalendarThreeTests.cs
@@ -0,0 +1,21 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class CalendarThreeTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            var calendar = new MyCalendarThree();
+
+            Assert.Equal(1, calendar.Book(10, 20)); // returns 1
+            Assert.Equal(1,calendar.Book(50, 60)); // returns 1
+            Assert.Equal(2,calendar.Book(10, 40)); // returns 2
+            Assert.Equal(3, calendar.Book(5, 15)); // returns 3
+            Assert.Equal(3, calendar.Book(5, 10)); // returns 3
+            Assert.Equal(3, calendar.Book(25, 55)); // returns 3
+        }
+    }
+}
