e4e9e3331013b38cac8e632cff4f614a952e2c20
Rajat Roy <rroy1@illumina.com>
2018-10-22 22:05:03 -0700

    new understanding of the problem. new solution. but bugs remain.

--- Problems/MyCalendarThree.cs
+++ Problems/MyCalendarThree.cs
@@ -15,13 +15,33 @@ namespace Problems
         public int Book(int start, int end)
         {
             var interval = new Interval(start, end);
-
-            var overlapCount = _tree.GetOverlappingIntervals(interval).Count();
+            var commonInterval = new Interval(start, end);
+            var overlapCount = 0;
+            foreach (var overlappingInterval in _tree.GetOverlappingIntervals(interval).OrderBy(x=>x.Start).ThenBy(x=>x.End))
+            {
+                commonInterval = Intersect(commonInterval, overlappingInterval);
+                if (commonInterval == null)
+                {
+                    if (_maxOverlap < overlapCount + 1) _maxOverlap = overlapCount + 1;
+                    overlapCount = 0;
+                    commonInterval = overlappingInterval;
+                }
+                else overlapCount++;
+            }
 
             _tree.Add(interval);
 
             if (_maxOverlap < overlapCount + 1) _maxOverlap = overlapCount + 1;
             return _maxOverlap;
+        }
+
+        private Interval Intersect(Interval interval, Interval overlappingInterval)
+        {
+            var start = interval.Start < overlappingInterval.Start ? overlappingInterval.Start : interval.Start;
+            var end =  interval.End < overlappingInterval.End ? interval.End : overlappingInterval.End;
+
+            if (start >= end) return null;
+            else return new Interval(start, end);
         }
     }
 }
--- UnitTests/CalendarThreeTests.cs
+++ UnitTests/CalendarThreeTests.cs
@@ -19,7 +19,7 @@ namespace UnitTests
         }
 
         [Fact]
-        public void Failed_case1()
+        public void Failed_case_6()
         {
             /*
              * input: ["MyCalendarThree","book","book","book","book","book","book","book","book","book","book"]
@@ -27,6 +27,36 @@ namespace UnitTests
              * Output:[null,1,1,2,2,3,4,4,4,5,5]
              * Expected:[null,1,1,2,2,3,3,3,3,4,4]
              */
+
+            var calendar = new MyCalendarThree();
+            var operations = new int[,] { {24, 40},{43, 50},{27, 43},{5, 21},{30, 40},{14, 29},{3, 19},{3, 14},{25, 39},{6, 19}};
+            var expected = new int[] { 1, 1, 2, 2, 3, 3, 3, 3, 4, 4 };
+            for (int i = 0; i < operations.GetLength(0); i++)
+            {
+                var result = calendar.Book(operations[i, 0], operations[i, 1]);
+                Assert.Equal(expected[i], result);
+            }
+        }
+
+        [Fact]
+        public void Failed_case_29()
+        {
+            /*
+             * input: ["MyCalendarThree","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book","book"]
+             * [[],[47,50],[1,10],[27,36],[40,47],[20,27],[15,23],[10,18],[27,36],[17,25],[8,17],[24,33],[23,28],[21,27],[47,50],[14,21],[26,32],[16,21],[2,7],[24,33],[6,13],[44,50],[33,39],[30,36],[6,15],[21,27],[49,50],[38,45],[4,12],[46,50],[13,21]]
+             * output:   [null,1,1,1,1,1,2,2,2,3,3,3,4,4,4,4,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6]
+             * expected: [null,1,1,1,1,1,2,2,2,3,3,3,4,5,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7]
+             */
+
+            var calendar = new MyCalendarThree();
+            var operations = new int[,] { {47, 50},{1, 10},{27, 36},{40, 47},{20, 27},{15, 23},{10, 18},{27, 36},{17, 25},{8, 17},{24, 33},{23, 28},{21, 27},{47, 50},{14, 21},{26, 32},{16, 21},{2, 7},{24, 33},{6, 13},{44, 50},{33, 39},{30, 36},{6, 15},{21, 27},{49, 50},{38, 45},{4, 12},{46, 50},{13, 21} };
+            var expected = new int[] { 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7 };
+
+            for (int i = 0; i < operations.GetLength(0); i++)
+            {
+                var result = calendar.Book(operations[i, 0], operations[i, 1]);
+                Assert.Equal(expected[i], result);
+            }
         }
     }
 
