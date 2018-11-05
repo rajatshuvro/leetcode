0d644d44a83273fa39b8cd16568b942d664adede
Rajat <rajatshuvro@gmail.com>
2018-11-04 21:51:54 -0800

    Ip address combinations

---
+++ Problems/IpCombinations.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    public class IpCombinations
+    {
+        //https://leetcode.com/problems/restore-ip-addresses/description/
+        public IList<string> RestoreIpAddresses(string s)
+        {
+            return GetValidIpTriplets(s, 4);
+        }
+
+        public IList<string> GetValidIpTriplets(string s, int tripletCount)
+        {
+            if (tripletCount <= 0 || string.IsNullOrEmpty(s)) return null;
+            if (s.Length < tripletCount || s.Length > 3 * tripletCount) return null;
+
+            var triplets = new List<string>();
+            for (int i = 0; i < 3 && i < s.Length; i++)
+            {
+                var triplet = s.Substring(0, i + 1);
+                if (int.Parse(triplet) <= 255)
+                {
+                    if (triplet == s)
+                    {
+                        triplets.Add(triplet);
+                        break;
+                    }
+
+                    var followingTriplets = GetValidIpTriplets(s.Substring(i + 1), tripletCount - 1);
+                    if (followingTriplets !=null) triplets.AddRange(followingTriplets.Select(x=>triplet+"."+x));
+                }
+            }
+
+            return triplets.Count>0? triplets: null;
+        }
+    }
+}
---
+++ UnitTests/IpCombinationsTests.cs
@@ -0,0 +1,68 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class IpCombinationsTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            var sol = new IpCombinations();
+
+            var combinations = sol.RestoreIpAddresses("25525511135");
+
+            Assert.Equal(new []{ "255.255.11.135", "255.255.111.35" }, combinations);
+        }
+
+        [Fact]
+        public void OneValidTriplet()
+        {
+            var sol = new IpCombinations();
+
+            var combinations = sol.GetValidIpTriplets("135",1);
+
+            Assert.Equal(new[] { "135" }, combinations);
+        }
+
+        [Fact]
+        public void OneInvalidTriplet()
+        {
+            var sol = new IpCombinations();
+
+            var combinations = sol.GetValidIpTriplets("1135", 1);
+
+            Assert.Null( combinations);
+        }
+
+        [Fact]
+        public void TwoValidTriplets_3()
+        {
+            var sol = new IpCombinations();
+
+            var combinations = sol.GetValidIpTriplets("1135", 2);
+
+            Assert.Equal(new[] { "1.135", "11.35", "113.5" }, combinations);
+        }
+
+        [Fact]
+        public void TwoValidTriplets_1()
+        {
+            var sol = new IpCombinations();
+
+            var combinations = sol.GetValidIpTriplets("52135", 2);
+
+            Assert.Equal(new[] { "52.135"}, combinations);
+        }
+
+        [Fact]
+        public void TwoValidTriplets_2()
+        {
+            var sol = new IpCombinations();
+
+            var combinations = sol.GetValidIpTriplets("2552", 2);
+
+            Assert.Equal(new[] { "25.52", "255.2" }, combinations);
+        }
+    }
+}
