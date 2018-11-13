11f0a9716dbfecd9c70f0b2dab10c3ebc57f45a6
Rajat Roy <rroy1@illumina.com>
2018-11-13 02:14:00 -0800

    recursively solving permutations

---
+++ Problems/Permutations.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Problems
+{
+    public class Permutations
+    {
+        //https://leetcode.com/problems/permutations/description/
+        private int[] _nums;
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            _nums = nums;
+            return GetPermutations(0, nums.Length);
+        }
+
+        private IList<IList<int>> GetPermutations(int i, int n)
+        {
+            var permutations = new List<IList<int>>();
+            if (n == 0) return permutations;
+            if (n == 1)
+            {
+                for(int j=i; j < _nums.Length; j++)
+                    permutations.Add(new List<int>(){_nums[j]});
+                return permutations;
+            }
+
+            for (int j = i; j < _nums.Length ; j++)
+            {
+                Swap(i,j);
+                foreach (var perm in GetPermutations(i+1,n-1))
+                {
+                    var permutation = new List<int>(){_nums[i]};
+                    permutation.AddRange(perm);
+                    permutations.Add(permutation);
+                }
+                //swapping back
+                Swap(i,j);
+
+            }
+            return permutations;
+
+        }
+
+        private void Swap( int i,  int j)
+        {
+            if (j == i) return;
+
+            var temp = _nums[i];
+            _nums[i] = _nums[j];
+            _nums[j] = temp;
+        }
+    }
+}
---
+++ UnitTests/PermutationsTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class PermutationsTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            var p = new Permutations();
+            var result = p.Permute(new[] {1, 2, 3});
+
+            var expected = new List<IList<int>>()
+            {
+                new List<int>() {1, 2, 3},
+                new List<int>() {1, 3, 2},
+                new List<int>() {2, 1, 3},
+                new List<int>() {2, 3, 1},
+                new List<int>() {3, 2, 1},
+                new List<int>() {3, 1, 2},
+            };
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
