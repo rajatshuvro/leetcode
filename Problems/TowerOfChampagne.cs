82dfe2a14e9c40ad66d35bdb4ebe7c1b8c5188d4
Rajat Roy <rroy1@illumina.com>
2018-11-20 22:38:50 -0800

    Correct but pretty bad performance

---
+++ Problems/TowerOfChampagne.cs
@@ -0,0 +1,40 @@
+﻿namespace Problems
+{
+    public class TowerOfChampagne
+    {
+        private const int MaxRowSize = 100;
+        private double[,] glasses= new double[MaxRowSize,MaxRowSize];
+
+        
+        public double ChampagneTower(int poured, int query_row, int query_glass)
+        {
+            if (query_row >= MaxRowSize || query_glass >= MaxRowSize) return 0;
+
+            glasses[0, 0] = poured;
+            for (int i = 0; i < MaxRowSize -1; i++)
+            {
+                bool noGlassOverflowing = true;
+                for (int j = 0; j <=i; j++)
+                {
+                    if (glasses[i, j] > 1)
+                    {
+                        noGlassOverflowing = false;
+                        glasses[i, j]--;
+                        glasses[i + 1, j] += glasses[i, j] / 2;
+                        glasses[i + 1, j + 1] += glasses[i, j] / 2;
+                        glasses[i, j] = 1;
+                    }
+                    
+                }
+                if (noGlassOverflowing) break;
+            }
+            //check the last row and spill to floor if overflowing
+            for (int i = 0; i < MaxRowSize; i++)
+            {
+                if (glasses[MaxRowSize - 1, i] > 1) glasses[MaxRowSize - 1, i] = 1;
+            }
+
+            return glasses[query_row, query_glass];
+        }
+    }
+}
---
+++ UnitTests/ChampagneTowerTests.cs
@@ -0,0 +1,79 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ChampagneTowerTests
+    {
+        [Fact]
+        public void Pour_1()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(1, 1, 1);
+
+            Assert.Equal(0.0, result);
+        }
+
+        [Fact]
+        public void Pour_2()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(2, 1, 1);
+
+            Assert.Equal(0.5, result);
+        }
+
+        [Fact]
+        public void Pour_3()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(3, 1, 1);
+
+            Assert.Equal(1.0, result);
+        }
+
+        [Fact]
+        public void Pour_4_2_0()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(4, 2, 0);
+
+            Assert.Equal(0.25, result);
+            
+        }
+
+        [Fact]
+        public void Pour_4_2_1()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(4, 2, 1);
+            Assert.Equal(0.5, result);
+            
+        }
+
+        [Fact]
+        public void Pour_4_2_2()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(4, 2, 2);
+            Assert.Equal(0.25, result);
+        }
+        [Fact]
+        public void Pour_6()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(6, 2, 1);
+
+            Assert.Equal(1.0, result);
+        }
+
+        [Fact]
+        public void Pour_200()
+        {
+            var champTower = new TowerOfChampagne();
+            var result = champTower.ChampagneTower(200, 15, 11);
+
+            Assert.Equal(1.0, result);
+        }
+    }
+}
