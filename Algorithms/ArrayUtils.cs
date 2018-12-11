0d320ae4595bcc6d1ebc2a23e3bf35ec44a57b33
Rajat Roy <rroy1@illumina.com>
2018-12-10 21:42:32 -0800

    adding array utils

---
+++ Algorithms/ArrayUtils.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms
+{
+    public static class ArrayUtils
+    {
+        public static int Partition(int[] nums, int i, int j, int pivotIndex)
+        {
+            if (i < 0 || i > nums.Length || j > nums.Length) return -1;
+            if (i > j || pivotIndex > j || i > pivotIndex) return -1;
+
+            var pivot = nums[pivotIndex];
+            while (i < j)
+            {
+                while (nums[i] < pivot) i++;
+                while (pivot < nums[j]) j--;
+
+                Swap(nums, i, j);
+
+                if (nums[i] == pivot) pivotIndex = i;
+                if (nums[j] == pivot) pivotIndex = j;
+            }
+
+            return pivotIndex;
+        }
+
+        public static void Swap(int[] nums, int i, int j)
+        {
+            if (j == i) return;
+
+            var temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
---
+++ Problems/FindMissingNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Problems
+{
+    public class FindMissingNumber
+    {
+        //https://leetcode.com/problems/missing-number/description/
+        private int[] _nums;
+        public int MissingNumber(int[] nums)
+        {
+            _nums = nums;
+            return MissingNumber(0, _nums.Length -1);
+        }
+
+        private int MissingNumber(int i, int j)
+        {
+            var mid = (i + j) / 2;
+            
+        }
+    }
+}
