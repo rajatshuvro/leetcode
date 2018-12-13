b2d71f5bc1aa86a892fa6e133c412d03cf6a2126
Rajat Roy <rroy1@illumina.com>
2018-12-12 21:58:11 -0800

    optimized o(n) but still at 30%

--- Problems/FindMissingNumber.cs
+++ Problems/FindMissingNumber.cs
@@ -8,39 +8,37 @@ namespace Problems
     {
         //https://leetcode.com/problems/missing-number/description/
 
-        public int MissingNumber(int[] nums)
-        {
-            Array.Sort(nums);
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (i != nums[i]) return i;
-            }
-
-            return nums.Length;
-        }
-        //private int[] _nums;
         //public int MissingNumber(int[] nums)
         //{
-        //    _nums = nums;
-        //    var max = nums.Max();
-        //    if ( max == nums.Length - 1) return nums.Length; 
-        //    return MissingNumber(0, _nums.Length -1);
-        //}
-
-        //private int MissingNumber(int i, int j)
-        //{
-        //    if (i == j)
+        //    Array.Sort(nums);
+        //    for (int i = 0; i < nums.Length; i++)
         //    {
-        //        return i == _nums[i] ? i + 1 : i;
+        //        if (i != nums[i]) return i;
         //    }
 
-        //    var pivotIndex = (i + j) / 2;
-        //    var pivot = _nums[pivotIndex];
-
-        //    pivotIndex = ArrayUtils.Partition(_nums, i, j, pivotIndex);
-
-        //    return pivotIndex == pivot ? MissingNumber(pivotIndex + 1, j) : MissingNumber(i, pivotIndex);
-
+        //    return nums.Length;
         //}
+        private int[] _nums;
+        public int MissingNumber(int[] nums)
+        {
+            _nums = nums;
+            return MissingNumber(0, _nums.Length - 1);
+        }
+
+        private int MissingNumber(int i, int j)
+        {
+            if (i >= j)
+            {
+                return j == _nums[j] ? j + 1 : j;
+            }
+
+            var pivotIndex = (i + j) / 2;
+            var pivot = _nums[pivotIndex];
+
+            pivotIndex = ArrayUtils.Partition(_nums, i, j, pivotIndex);
+
+            return pivotIndex == pivot ? MissingNumber(pivotIndex + 1, j) : MissingNumber(i, pivotIndex);
+
+        }
     }
 }
