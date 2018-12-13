d9acdf84d4e2f32c732428f7d8ecb0cb214c810b
Rajat Roy <rroy1@illumina.com>
2018-12-12 21:51:26 -0800

    simple sorting solution gives 43%

--- Problems/FindMissingNumber.cs
+++ Problems/FindMissingNumber.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Algorithms;
 
 namespace Problems
@@ -6,29 +7,40 @@ namespace Problems
     public class FindMissingNumber
     {
         //https://leetcode.com/problems/missing-number/description/
-        private int[] _nums;
+
         public int MissingNumber(int[] nums)
         {
-            _nums = nums;
-            var max = nums.Max();
-            if ( max == nums.Length - 1) return nums.Length; 
-            return MissingNumber(0, _nums.Length -1);
-        }
-
-        private int MissingNumber(int i, int j)
-        {
-            if (i == j)
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i++)
             {
-                return i == _nums[i] ? i + 1 : i;
+                if (i != nums[i]) return i;
             }
 
-            var pivotIndex = (i + j) / 2;
-            var pivot = _nums[pivotIndex];
-
-            pivotIndex = ArrayUtils.Partition(_nums, i, j, pivotIndex);
-
-            return pivotIndex == pivot ? MissingNumber(pivotIndex + 1, j) : MissingNumber(i, pivotIndex);
-
+            return nums.Length;
         }
+        //private int[] _nums;
+        //public int MissingNumber(int[] nums)
+        //{
+        //    _nums = nums;
+        //    var max = nums.Max();
+        //    if ( max == nums.Length - 1) return nums.Length; 
+        //    return MissingNumber(0, _nums.Length -1);
+        //}
+
+        //private int MissingNumber(int i, int j)
+        //{
+        //    if (i == j)
+        //    {
+        //        return i == _nums[i] ? i + 1 : i;
+        //    }
+
+        //    var pivotIndex = (i + j) / 2;
+        //    var pivot = _nums[pivotIndex];
+
+        //    pivotIndex = ArrayUtils.Partition(_nums, i, j, pivotIndex);
+
+        //    return pivotIndex == pivot ? MissingNumber(pivotIndex + 1, j) : MissingNumber(i, pivotIndex);
+
+        //}
     }
 }
