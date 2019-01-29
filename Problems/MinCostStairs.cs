a82115b2813b252e70f2f85d37176ae4c0757398
Rajat Roy <rroy1@illumina.com>
2019-01-28 21:04:59 -0800

    Best solution so far :)

---
+++ Problems/MinCostStairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DataStructures;
+
+namespace Problems
+{
+    public class MinCostStairs
+    {
+        //https://leetcode.com/problems/min-cost-climbing-stairs/description/
+        //On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+        //Once you pay the cost, you can either climb one or two steps.You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+        public int MinCostClimbingStairs(int[] stepCost)
+        {
+            var n = stepCost.Length;
+            if (n == 1) return 0;
+            if (n == 2) return Math.Min(stepCost[0], stepCost[1]);
+            var climbingCost = new int[n];
+
+            climbingCost[n - 1] = stepCost[n - 1];
+            climbingCost[n - 2] = stepCost[n - 2];
+            
+            for (int i = n - 3; i >= 0; i--)
+            {
+                climbingCost[i] = stepCost[i] + Math.Min(climbingCost[i + 1], climbingCost[i + 2]);
+            }
+
+            return Math.Min(climbingCost[0], climbingCost[1]);
+        }
+    }
+}
---
+++ UnitTests/ClimbingStairsTests.cs
@@ -0,0 +1,18 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ClimbingStairsTests
+    {
+        [Theory]
+        [InlineData(new int[] { 10, 15, 20 }, 15)]
+        [InlineData(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6)]
+        public void Test_1(int[] costs, int expectedResult)
+        {
+            var sol = new MinCostStairs();
+
+            Assert.Equal(expectedResult, sol.MinCostClimbingStairs(costs));
+        }
+    }
+}
