a4589b1f509f317b9df10fd0be6d335dac80dd72
Rajat Roy <rroy1@illumina.com>
2019-06-01 11:15:46 -0700

    creating tests

---
+++ UnitTests/ExamRoomTests.cs
@@ -0,0 +1,47 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ExamRoomTests
+    {
+        [Fact]
+        public void Seat_nominal()
+        {
+            var examRoom = new ExamRoom(10);
+
+            Assert.Equal(0,examRoom.Seat());
+            Assert.Equal(9, examRoom.Seat());
+            Assert.Equal(4, examRoom.Seat());
+            Assert.Equal(6, examRoom.Seat());
+        }
+
+        [Fact]
+        public void Leave_nominal()
+        {
+            var examRoom = new ExamRoom(10);
+
+            Assert.Equal(0, examRoom.Seat());
+            Assert.Equal(9, examRoom.Seat());
+            Assert.Equal(4, examRoom.Seat());
+            Assert.Equal(6, examRoom.Seat());
+
+            examRoom.Leave(4);
+            Assert.Equal(3, examRoom.Seat());
+        }
+
+        [Fact]
+        public void Leave_at_boundaries()
+        {
+            var examRoom = new ExamRoom(10);
+
+            Assert.Equal(0, examRoom.Seat());
+            Assert.Equal(9, examRoom.Seat());
+            Assert.Equal(4, examRoom.Seat());
+            Assert.Equal(6, examRoom.Seat());
+
+            examRoom.Leave(0);
+            Assert.Equal(0, examRoom.Seat());
+        }
+    }
+}
