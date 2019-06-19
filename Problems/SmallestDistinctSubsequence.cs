4c86a452ed4c158b5f1578a8bbe108379e58d587
Rajat Roy <rroy1@illumina.com>
2019-06-19 04:37:00 -0700

    time limit exceeded on large cases.

--- Problems/SmallestDistinctSubsequence.cs
+++ Problems/SmallestDistinctSubsequence.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Problems
 {
@@ -8,27 +9,68 @@ namespace Problems
     {
         //https://leetcode.com/contest/weekly-contest-140/problems/smallest-subsequence-of-distinct-characters/
         //Return the lexicographically smallest subsequence of text that contains all the distinct characters of text exactly once
+        private Dictionary<(int, string), string> _memoizationTable = new Dictionary<(int, string), string>();
+        private string _text;
+        private HashSet<char> _charSet;
         public string SmallestSubsequence(string text)
         {
-            return SmallestSubsequence(text, 0, new HashSet<char>());
+            if (string.IsNullOrEmpty(text)) return null;
+
+            _text = text;
+
+            _charSet = new HashSet<char>(text.Length);
+            foreach (var c in text)
+            {
+                _charSet.Add(c);
+            }
+            return SmallestSubsequence(0);
         }
 
-        private string SmallestSubsequence(string text, int i, HashSet<char> exclude)
+        private string SmallestSubsequence(int i)
         {
-            string s1, s2;
-            if (!exclude.Contains(text[i]))
+            string s1=null;
+            if (_charSet.Count == 0 || i >=_text.Length) return null;
+            if (i == _text.Length - 1 && _charSet.Contains(_text[i])) return _text.Substring(i);
+
+            var sortedCharSet = GetSortedCharSet();
+            if (_memoizationTable.TryGetValue((i, sortedCharSet), out var result)) return result;
+
+            // assume char i will be the next one in the subsequence
+            if (_charSet.Contains(_text[i]))
             {
-                exclude.Add(text[i]);
-                s1 = text[i] + SmallestSubsequence(text, i + 1, exclude);
-                exclude.Remove(text[i]);
+                _charSet.Remove(_text[i]);
+                s1 = _text[i] + SmallestSubsequence(i + 1);
+                _charSet.Add(_text[i]);
             }
-            else
+            
+            var s2 = SmallestSubsequence(i + 1);
+            var smallestSub = LexiCompare(s1, s2);
+            if (smallestSub !=null && smallestSub.Length != _charSet.Count) smallestSub = null;
+
+            _memoizationTable[(i, sortedCharSet)] = smallestSub;
+
+            return smallestSub;
+
+        }
+
+        private string LexiCompare(string s1, string s2)
+        {
+            if (s1 == null) return s2;
+            if (s2 == null) return s1;
+            //if (s1.Length > s2.Length) return s1;
+            //if (s2.Length > s1.Length) return s2;
+            return string.CompareOrdinal(s1, s2) < 0 ? s1 : s2;
+        }
+
+        private string GetSortedCharSet()
+        {
+            var chars = new StringBuilder();
+            foreach (var c in _charSet.OrderBy(x => x))
             {
-                s2 = SmallestSubsequence(text, i + 1, exclude);
+                chars.Append(c);
             }
 
-            return s1.CompareTo(s2) < 0 ? s1 : s2;
-
+            return chars.ToString();
         }
     }
 }
--- UnitTests/SmallestDistinctSubsequenceTests.cs
+++ UnitTests/SmallestDistinctSubsequenceTests.cs
@@ -10,6 +10,7 @@ namespace UnitTests
         [InlineData("abcd", "abcd")]
         [InlineData("ecbacba", "eacb")]
         [InlineData("leetcode", "letcod")]
+        [InlineData("gicbjijdihjidagibifbeejgjjhfhjihcaihabhihjbiihaadijfgjdjjagcdaehcjhcfeffjhfjbghbbeageabhhjgfdihhbfjafaachibjaejegbbciejageigdeihhhbeejdahcgchjgebgcfgajeddebjfffjdgejggdjddjjihfihbcdddbjcbhdeedfhhghaddeggededfegjeffaejbfefbiighjacaecgdbhhihjbhegffhaef", "time limit exceeded")]
         public void Examples(string text, string result)
         {
             var sol = new SmallestDistinctSubsequence();
