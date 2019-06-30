828e5d5a9f3e857ec71fd50e01d31aca5963abbf
Rajat Roy <rroy1@illumina.com>
2019-06-29 22:47:52 -0700

    Next Greater Number problem

---
+++ Problems/NextGreaterNumber.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using DataStructures;
+
+namespace Problems
+{
+    public class NextGreaterNumber
+    {
+        public class ValueAndIndex
+        {
+            public readonly int Index;
+            public readonly int Value;
+
+            public ValueAndIndex(int i, int value)
+            {
+                Index = i;
+                Value = value;
+            }
+        }
+
+        public int[] NextLargerNodes(ListNode head)
+        {
+            var nextLarger = new List<int>(10000);
+            // unassigned is an ascending list of items that haven't been assigned a larger node.
+            var unassigned = new LinkedList<ValueAndIndex>();
+
+            while (head != null)
+            {
+                nextLarger.Add(0);
+                unassigned.AddFirst(new ValueAndIndex(nextLarger.Count-1, head.val));
+
+                if (head.next != null && head.next.val > head.val)
+                {
+                    var risingValue = head.next.val;
+                    
+                    while (unassigned.Count > 0)
+                    {
+                        var value = unassigned.First.Value.Value;
+                        var index = unassigned.First.Value.Index;
+
+                        if (value >= risingValue) break;
+
+                        nextLarger[index]=risingValue;
+                        unassigned.RemoveFirst();
+                    }
+                    
+                }
+
+                head = head.next;
+            }
+
+            return nextLarger.ToArray();
+        }
+    }
+}
---
+++ UnitTests/NextLargerNodeTests.cs
@@ -0,0 +1,27 @@
+﻿using DataStructures;
+using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class NextLargerNodeTests
+    {
+        [Theory]
+        [InlineData(new[] { 2 }, new[] { 0 })]
+        [InlineData(new []{ 2, 1, 5 }, new[] { 5, 5, 0 })]
+        [InlineData(new[] { 2, 7, 4, 3, 5 }, new[] { 7, 0, 5, 5, 0 })]
+        [InlineData(new[] { 1, 7, 5, 1, 9, 2, 5, 1 }, new[] { 7, 9, 9, 9, 0, 5, 0, 0 })]
+        public void NextGreaterNumberTests(int[] input, int[] expectedOutput)
+        {
+            var greaterNumberFinder = new NextGreaterNumber();
+            var linkedList = new LinkedList();
+            foreach (var i in input)
+            {
+                linkedList.Add(new ListNode(i));
+            }
+
+            var results = greaterNumberFinder.NextLargerNodes(linkedList.First);
+            Assert.Equal(expectedOutput, results);
+        }
+    }
+}
