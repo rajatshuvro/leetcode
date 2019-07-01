5e88f6132471adc54c27ee6aa99ab87a6b4922a8
Rajat Roy <rroy1@illumina.com>
2019-07-01 15:19:08 -0700

    first reservoir sampling problem

---
+++ Problems/LinkedListRandomNode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataStructures;
+
+namespace Problems
+{
+    public class LinkedListRandomNode
+    {
+        /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+        private readonly ListNode _head;
+        private ListNode _current;
+        private readonly Random _random;
+        private readonly List<int> _reservoir;
+        private const int ReservoirCapacity = 20;
+        private int _linkedListIndex;
+        public LinkedListRandomNode(ListNode head)
+        {
+            _head    = head;
+            _current = head;
+            _reservoir = InitializeReservoir();
+            _random  = new Random();
+        }
+
+        private List<int> InitializeReservoir()
+        {
+            var reservoir = new List<int>();
+            _linkedListIndex = 0; 
+            while (_current != null && _linkedListIndex < ReservoirCapacity)
+            {
+                reservoir.Add(_current.val);
+                _current = _current.next;
+                _linkedListIndex++;
+            }
+
+            return reservoir;
+        }
+
+        private int GetRandomFromReservoir()
+        {
+            var i = _random.Next(0, _reservoir.Count);
+            return _reservoir[i];
+        }
+
+        /** Returns a random node's value. */
+        public int GetRandom()
+        {
+            if (_reservoir.Count < ReservoirCapacity) return GetRandomFromReservoir();
+
+            if (_current == null) _current = _head;
+
+            //reservoir did not fill up
+            TryAddToReservoir(_current.val);
+            _current = _current.next;
+
+            return GetRandomFromReservoir();
+        }
+
+        private void TryAddToReservoir(int value)
+        {
+            if (_reservoir.Count < ReservoirCapacity)
+            {
+                _reservoir.Add(value);
+                return;
+            }
+
+            //reservoir full, we need to replace an item
+            var i = _random.Next(0, _reservoir.Count);
+            _linkedListIndex++;
+            if (_random.NextDouble() < 1.0 / _linkedListIndex) _reservoir[i] = value;
+        }
+    }
+}
---
+++ UnitTests/LinkedListRandomNodeTester.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using DataStructures;
+using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class LinkedListRandomNodeTester
+    {
+        [Theory]
+        [InlineData(new[] { 10, 100, 100, 20, 20, 100 })]
+        [InlineData(new[] { 10, 1, 10, 20, 100 })]
+        public void TestRandomness(int[] numbers)
+        {
+            var inputFreq = GetFrequencies(numbers);
+            var linkedList = new LinkedList();
+            foreach (var number in numbers)
+            {
+                linkedList.Add(new ListNode(number));
+            }
+
+            var randomNodeGenerator = new LinkedListRandomNode(linkedList.First);
+            var randomNodeFrequencies = GetRandomNodeFreq(randomNodeGenerator, numbers.Length);
+
+            Assert.Equal(inputFreq.Count, randomNodeFrequencies.Count);
+            foreach (var (number, frequency) in inputFreq)
+            {
+                var observedFreq = randomNodeFrequencies[number];
+                var freqRatio = frequency > observedFreq ? observedFreq / frequency : frequency / observedFreq;
+                Assert.True(freqRatio > 0.95);
+            }
+        }
+
+        private Dictionary<int, double> GetRandomNodeFreq(LinkedListRandomNode randomNodeGenerator, int count)
+        {
+            var array = new int[count * 100];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomNodeGenerator.GetRandom();
+            }
+
+            return GetFrequencies(array);
+        }
+
+        private Dictionary<int, double> GetFrequencies(int[] numbers)
+        {
+            var counts = new Dictionary<int, double>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                    counts[number] += 1;
+                else counts[number] = 1;
+            }
+
+            var frequencies = new Dictionary<int, double>();
+            foreach (var (number, count) in counts)
+            {
+                frequencies[number] = count / numbers.Length;
+            }
+
+            return frequencies;
+        }
+    }
+}
