1b065e43f1482509f233052079a1b38d59ec4180
Rajat Roy <rroy1@illumina.com>
2019-06-30 23:07:46 -0700

    random points in circle

---
+++ Problems/RandomPointsInCircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problems
+{
+    public class RandomPointsInCircle
+    {
+        //https://leetcode.com/problems/generate-random-point-in-a-circle/
+
+        private readonly double _radius;
+        private readonly double _xCenter;
+        private readonly double _yCenter;
+        public RandomPointsInCircle(double radius, double x_center, double y_center)
+        {
+            _radius = radius;
+            _xCenter = x_center;
+            _yCenter = y_center;
+        }
+
+        public double[] RandPoint()
+        {
+            var rand = new Random();
+
+            var x = rand.NextDouble() * _radius * 2 + (_xCenter - _radius);
+            var yRange = Math.Sqrt(_radius * _radius - (x - _xCenter) * (x - _xCenter));
+            var y = rand.NextDouble() * yRange * 2 + (_yCenter - yRange);
+
+            return new[] {x, y};
+        }
+    }
+}
---
+++ UnitTests/RandomCirclePointsTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class RandomCirclePointsTests
+    {
+        [Theory]
+        //[InlineData(1,0,0)]
+        [InlineData(10, 5.0, -7.50)]
+        public void ArePointsInCircle(double radius, double xCenter, double yCenter)
+        {
+            var sol = new RandomPointsInCircle(radius, xCenter, yCenter);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var xy = sol.RandPoint();
+                var radiusSquare = radius * radius;
+                var xSquare = (xy[0]- xCenter) * (xy[0] - xCenter);
+                var ySquare = (xy[1] - yCenter) * (xy[1] - yCenter);
+                Assert.True(radiusSquare.CompareTo(xSquare+ySquare) > 0);
+            }
+        }
+    }
+}
