d6c4586a5a8a7eac29cd58d9c6ecff6c6af96fee
Rajat Roy <rroy1@illumina.com>
2019-07-20 09:09:21 -0700

    Concurrency problem 1: print order

---
+++ Problems/PrintOrder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace Problems
+{
+    public class PrintOrder
+    {
+        //https://leetcode.com/problems/print-in-order/
+        // not available for c#. Just coding for practice
+        private readonly Semaphore _semaphore1, _semaphore2;
+        private readonly StringBuilder _sb;
+
+        public PrintOrder()
+        {
+            _sb = new StringBuilder();
+            _semaphore1 = new Semaphore(0,3);
+            _semaphore2 = new Semaphore(0,3);
+        }
+
+        public void First()
+        {
+            _sb.Append("first");
+            _semaphore1.Release();
+        }
+
+        public void Second()
+        {
+            _semaphore1.WaitOne();
+            _sb.Append("second");
+            _semaphore2.Release();
+        }
+
+        public void Third()
+        {
+            _semaphore2.WaitOne();
+            _sb.Append("third");
+        }
+
+        public string GetOutput()
+        {
+            return _sb.ToString();
+        }
+    }
+}
---
+++ UnitTests/PrintOrderTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using Problems;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class PrintOrderTests
+    {
+        [Fact]
+        public void ThreeTwoOne()
+        {
+            var printer = new PrintOrder();
+            var thread1 = new Thread(() => printer.First());
+            var thread2 = new Thread(() => printer.Second());
+            var thread3 = new Thread(() => printer.Third());
+
+            thread3.Start();
+            thread2.Start();
+            thread1.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Assert.Equal("firstsecondthird", printer.GetOutput());
+        }
+
+        [Fact]
+        public void TwoThreeOne()
+        {
+            var printer = new PrintOrder();
+            var thread1 = new Thread(() => printer.First());
+            var thread2 = new Thread(() => printer.Second());
+            var thread3 = new Thread(() => printer.Third());
+
+            thread2.Start();
+            thread3.Start();
+            thread1.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Assert.Equal("firstsecondthird", printer.GetOutput());
+        }
+
+        [Fact]
+        public void OneThreeTwo()
+        {
+            var printer = new PrintOrder();
+            var thread1 = new Thread(() => printer.First());
+            var thread2 = new Thread(() => printer.Second());
+            var thread3 = new Thread(() => printer.Third());
+
+            thread1.Start();
+            thread3.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Assert.Equal("firstsecondthird", printer.GetOutput());
+        }
+    }
+}
