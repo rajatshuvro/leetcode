158ca785ec057b120213fda18e827e6637cdcf50
Rajat Roy <rroy1@illumina.com>
2019-07-24 06:36:29 -0700

    new fun problems

---
+++ Problems/EfficientPower.cs
@@ -0,0 +1,12 @@
+﻿namespace Problems
+{
+    public static class EfficientPower
+    {
+        // interview problem
+        // more efficient power function
+        public static double Power(double x, int y)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Problems/EnumeratorOfEnumerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Problems
+{
+    public class EnumeratorOfEnumerator<T,K>:IEnumerator<T>
+    {
+        // interview questions
+        // implement an enumerator of T, that takes an array of K and a function that returns an enumerator of T given K
+        // e.g. given ["abc", "def"] return ['a', 'b', 'c', 'd', 'e', 'f']
+
+        private Func<K, IEnumerable<T>> _enumFunc;
+        public EnumeratorOfEnumerator(K[] array, Func<K, IEnumerable<T>> enumFunc)
+        {
+
+        }
+        public bool MoveNext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Current { get; }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
