461019d3618b2fa7abd036ccf71566710b268cea
Rajat Roy <rroy1@illumina.com>
2019-07-27 16:40:25 -0700

    Efficient POW implementation

--- Problems/EfficientPower.cs
+++ Problems/EfficientPower.cs
@@ -6,7 +6,12 @@
         // more efficient power function
         public static double Power(double x, int y)
         {
-            return 0;
+            if (y == 0) return 1;
+            if (y == 1) return x;
+
+            double r1 = Power(x, y / 2);
+            if (y % 2 == 0) return r1 * r1;
+            return r1 * r1 * x;
         }
     }
 }
---
+++ UnitTests/PowerTests.cs
@@ -0,0 +1,16 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class PowerTests
+    {
+        [Theory]
+        [InlineData(4.5, 5, 1845.28125)]
+        [InlineData(1.5, 15, 437.893890380859375)]
+        public void BasicTheory(double x, int y, double result)
+        {
+            Assert.Equal(result, EfficientPower.Power(x,y));
+        }
+    }
+}
