cba4e60f4ad2294c7021354d4d53284286840da3
Rajat Roy <rroy1@illumina.com>
2019-07-29 22:09:25 -0700

    last problem of efficient sum of digits

---
+++ Problems/SumOfDigits.cs
@@ -0,0 +1,42 @@
+﻿namespace Problems
+{
+    public class SumOfDigits
+    {
+        // interview problem
+        // given an integer, return sum of digits
+        // you may create a look up table of partial sums. Memory is limited
+
+        private readonly byte[] _partialSums;
+
+        public SumOfDigits()
+        {
+            _partialSums = new byte[1000];
+            for (uint i = 0; i < 1000; i++)
+                _partialSums[i] = (byte)SumAllDigits(i);
+        }
+
+        public uint EfficientSum(uint x)
+        {
+            uint sum = 0;
+            while (x > 0)
+            {
+                var i = x % 1000;
+                sum += _partialSums[i];
+                x /= 1000;
+            }
+
+            return sum;
+        }
+        public uint SumAllDigits(uint x)
+        {
+            uint sum = 0;
+            while (x > 0)
+            {
+                sum += (x % 10);
+                x /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ UnitTests/SumOfDigitsTests.cs
@@ -0,0 +1,19 @@
+﻿using Problems;
+using Xunit;
+
+namespace UnitTests
+{
+    public class SumOfDigitsTests
+    {
+        [Theory]
+        [InlineData(123, 6)]
+        [InlineData(145783, 28)]
+        [InlineData(3_798_145_783, 55)]
+        public void TestUints(uint x, uint sum)
+        {
+            var digitAdder = new SumOfDigits();
+
+            Assert.Equal(sum, digitAdder.EfficientSum(x));
+        }
+    }
+}
