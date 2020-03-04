f6f4ae909a2068c7b07805782eddb50a7b66292a
Roy <rroy1@USSD-OLM-039070.local>
2020-03-04 06:26:33 -0800

    starting with an easy problem

---
+++ Problems/BinSearchTreeMode.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataStructures;
+
+namespace Problems
+{
+    public class BinSearchTreeMode
+    {
+        private Dictionary<int, int> _valueCounts = new Dictionary<int, int>();
+        public int[] FindMode(TreeNode root)
+        {
+            if(root == null) return Array.Empty<int>();
+            TraverseBst(root);
+            var maxCount = _valueCounts.Values.Max();
+            var modes = new List<int>();
+            foreach (var (key, value) in _valueCounts)
+            {
+                if(value == maxCount) modes.Add(key);
+            }
+
+            return modes.ToArray();
+        }
+
+        private void TraverseBst(TreeNode root)
+        {
+            if (root == null) return;
+            if (_valueCounts.ContainsKey(root.val)) _valueCounts[root.val]++;
+            else _valueCounts.Add(root.val, 1);
+            
+            if(root.left != null) TraverseBst(root.left);
+            if(root.right != null) TraverseBst(root.right);
+        }
+    }
+}
