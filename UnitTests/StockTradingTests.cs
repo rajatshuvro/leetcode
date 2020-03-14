54819f0d90a22eabb34f017c21df5f4c83dce4a3
Roy <rroy1@USSD-OLM-039070.local>
2020-03-14 16:36:06 -0700

    redoing daily stock trader

--- UnitTests/StockTradingTests.cs
+++ UnitTests/StockTradingTests.cs
@@ -13,5 +13,16 @@ namespace UnitTests
             var oneTimeTrader = new OneTimeTrader();
             Assert.Equal(maxProfit, oneTimeTrader.MaxProfit(prices));
         }
+
+        [Theory]
+        [InlineData(new[]{7,1,5,3,6,4}, 7)]
+        [InlineData(new[]{7,5,3,1,3,5,4,3,4,6,5,4}, 7)]
+        [InlineData(new[]{1,2,3,4,5}, 4)]
+        [InlineData(new[]{7,6,4,3,1}, 0)]
+        public void DailyStockTraderTests(int[] prices, int maxProfit)
+        {
+            var dailyTrader = new DailyStockTrader();
+            Assert.Equal(maxProfit, dailyTrader.MaxProfit(prices));
+        }
     }
 }
