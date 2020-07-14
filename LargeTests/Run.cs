2deb5113775ba919f01dbee41e60d386c72a3c21
Rajat Roy <rroy1@illumina.com>
2020-07-14 14:49:10 -0700

    updating output messaging

--- LargeTests/Run.cs
+++ LargeTests/Run.cs
@@ -25,10 +25,10 @@ namespace LargeTests
         {
             Console.WriteLine("Testing interval cover");
             var min = 1;
-            var max = 2_000_000_000;
-            var count = 3_000_000;
+            var max = 250_000_000;
+            var count = 300_000;
             var largeIntervalSize = 20 * 16 * 1024;
-            var smallIntervalSize = 20 * 8 * 1024;
+            var smallIntervalSize = 20 * 2 * 1024;
             
             var nums = IntervalCoverUtilities.GetUniformRandomNums(count, min, max);
             
@@ -36,18 +36,24 @@ namespace LargeTests
             var smallIntervals = IntervalCoverUtilities.GetIntervals(min, max, smallIntervalSize);
 
             Console.WriteLine($"Using {largeIntervals.Count} large and {smallIntervals.Count} small intervals.");
-
+            Console.WriteLine($"Testing grouped numbers. count {nums.Length}");
+            Console.Write("Using both large and small intervals ");
             IntervalCoverUtilities.BenchmarkIntervalCover(nums, largeIntervals, smallIntervals );
+            Console.Write("Using large intervals ");
             IntervalCoverUtilities.BenchmarkIntervalCover(nums, largeIntervals);
+            Console.Write("Using small intervals ");
             IntervalCoverUtilities.BenchmarkIntervalCover(nums, smallIntervals);
 
-            Console.WriteLine("Testing grouped uniform distributions");
-            var groupSize =  20_000;
-            var gapSize = 1_000_000;
+            var groupSize =  10_000;
+            var gapSize = 500_000;
             var groupedNums = IntervalCoverUtilities.GetGroupedNums(count, groupSize, gapSize, min, max);
-            
+            Console.WriteLine($"Testing grouped numbers. count {groupedNums.Length}");
+
+            Console.Write("Using both large and small intervals ");
             IntervalCoverUtilities.BenchmarkIntervalCover(groupedNums, largeIntervals, smallIntervals );
+            Console.Write("Using large intervals ");
             IntervalCoverUtilities.BenchmarkIntervalCover(groupedNums, largeIntervals);
+            Console.Write("Using small intervals ");
             IntervalCoverUtilities.BenchmarkIntervalCover(groupedNums, smallIntervals);
         }
     }
