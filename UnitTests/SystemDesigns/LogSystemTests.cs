729394fd74c6dc7cfdcbd21edc2b8bfc4633e0d9
Rajat Roy <rroy1@illumina.com>
2020-07-24 15:42:31 -0700

    basic test cases working

--- UnitTests/SystemDesigns/LogSystemTests.cs
+++ UnitTests/SystemDesigns/LogSystemTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Problems.SystemDesigns;
 using Xunit;
 
@@ -27,6 +28,23 @@ namespace UnitTests.SystemDesigns
         public void RoundDownTimeTests(string time, string granularity, string expected)
         {
             Assert.Equal(expected, LogSystemUtilities.GetRoundDownTime(time, granularity));
+        }
+
+        [Fact]
+        public void Case_0()
+        {
+            var ls = new LogSystem();
+            ls.Put(1, "2017:01:01:23:59:59");
+            ls.Put(2, "2017:01:01:22:59:59");
+            ls.Put(3, "2016:01:01:00:00:00");
+
+            var result = ls.Retrieve("2016:01:01:01:01:01", "2017:01:01:23:00:00", "Year").ToList();
+            result.Sort();
+            Assert.Equal(new []{1,2,3}, result);
+
+            result = ls.Retrieve("2016:01:01:01:01:01", "2017:01:01:23:00:00", "Hour").ToList();
+            result.Sort();
+            Assert.Equal(new []{1,2}, result);
         }
     }
 }
