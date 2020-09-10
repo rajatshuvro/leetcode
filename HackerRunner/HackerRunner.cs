c2c1310bb5b78c32d91460535b8e444cc79ae5a1
Rajat Roy <rroy1@illumina.com>
2020-09-10 14:57:09 -0700

    fixing the hacker rank runner

--- HackerRunner/HackerRunner.cs
+++ HackerRunner/HackerRunner.cs
@@ -18,23 +18,20 @@ namespace HackerRunner
             int t = Convert.ToInt32(reader.ReadLine());
 
             string line=null;
-            try
-            {
-                for (int tItr = 0; tItr < t; tItr++) {
-                    int money = Convert.ToInt32(reader.ReadLine());
+            for (int tItr = 0; tItr < t; tItr++) {
+                int money = Convert.ToInt32(reader.ReadLine());
 
-                    int n = Convert.ToInt32(reader.ReadLine());
+                int n = Convert.ToInt32(reader.ReadLine());
 
-                    line = reader.ReadLine();
-                    int[] cost = Array.ConvertAll(line.Split(' '), costTemp => Convert.ToInt32(costTemp))
-                        ;
-                    whatFlavors(cost, money);
+                line = reader.ReadLine();
+                var nums = line.Split(' ');
+                //int[] cost = Array.ConvertAll(line.Split(' '), costTemp => Convert.ToInt32(costTemp));
+                var cost = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    cost[i] = Convert.ToInt32(nums[i]);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error in line:{line}");
-                throw;
+                whatFlavors(cost, money);
             }
             
         }
